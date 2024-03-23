7b2156d552f273adcdc61e9288c9ed45438ad56d
gitshubham45 <shubham16290@gmail.com>
2024-03-24 02:29:43 +0530

    user models creaed along with updateUser function

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -21,7 +21,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Image from 'next/image';
 import { isBase64Image } from '@/lib/utils';
 import { uploadFiles, useUploadThing } from '@/lib/uploadthing';
-
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
   user: {
@@ -38,7 +39,9 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
   const [files, setFiles] = useState<File[]>([]);
-  const { startUpload } = useUploadThing("media")
+  const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -103,10 +106,19 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         });
 
     }
-    
-    
 
-    
+
+
+    await updateUser(
+      {
+        userId: user.id,
+        username: values.username,
+        name: values.name,
+        bio: values.bio,
+        image: values.profile_photo, 
+        path: pathname
+      }
+    )
     //TODO : update user profile
 
 
